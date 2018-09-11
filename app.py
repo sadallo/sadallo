0fc4b63ad51888a0616a3deac961f681df012431
sadallo <sadallo@hotmail.com>
2018-09-11 20:08:29 -0300

    commit

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, g
+from functools import wraps
+import sqlite3
 
 app = Flask(__name__)
 
+app.database = "sample.db"
+
+
 @app.route('/')
-def index():
-	return 'Projeto Sadallo!'
+def home():
+	g.db = connect_db()
+	cur = g.db.execute('select * from posts')
+	posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+	g.db.close()
+	return render_template("index.html", posts=posts)
+
+@app.route('/universidade/<sigla>/')
+def universidade_busca(sigla):
+	return 'vou pesquisar %s' % sigla
+
+@app.route('/universidade/')
+def universidade():
+	return 'Insira uma sigla de alguma universidade federal na url. <br>Exemplo: sadallo.herokuapp.com/universidade/ufmg'
+
+'''@app.route('/welcome/')
+def welcome():
+	return render_template("welcome.html")'''
+def connect_db():
+	return sqlite3.connect(app.database)
+
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
