0e0d399d0373e3cf983f361c6937473f3b715113
sadallo <sadallo@hotmail.com>
2018-09-11 20:10:16 -0300

    commit terrible

--- sql.py
+++ sql.py
@@ -7,3 +7,4 @@ with sqlite3.connect("sample.db") as connection:
 	c.execute('INSERT INTO posts VALUES("Well", "I\'m well")')
 	c.execute('INSERT INTO posts VALUES("Good", "I\'m good")')
 	c.execute('INSERT INTO posts VALUES("Bad", "I\'m bad")')
+	c.execute('INSERT INTO posts VALUES("Terrible", "I\'m terrible")')
